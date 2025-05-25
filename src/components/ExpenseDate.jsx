5a233a907e2446a9773f4a285410ecd761f505b6
Simon <omojide.o.j@gmail.com>
2025-05-25 21:22:18 +0100

    Add Expenses.jsx

--- src/components/ExpenseDate.jsx
+++ src/components/ExpenseDate.jsx
@@ -1,7 +1,7 @@
 import './ExpenseDate.css'
 
 const ExpenseDate = ({ date }) => {
-	// Helper
+	// Helper functions
 	const month = date.toLocaleString('en-GB', { month: 'long' });
 	const day = date.toLocaleString('en-GB', { day: '2-digit' });
 	const year = date.getFullYear();
--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -1,9 +1,10 @@
 import ExpenseItem from './ExpenseItem';
+import Card from './Card';
 import './Expenses.css';
 
 const Expenses = (props) => {
 	return (
-		<div className='expenses'>
+		<Card className='expenses'>
 			<ExpenseItem
 				title={props.items[0].title}
 				amount={props.items[0].amount}
@@ -25,7 +26,7 @@ const Expenses = (props) => {
 				date={props.items[3].date}
 			/>
 			<p>Hello World</p>
-		</div>
+		</Card>
 	);
 };
 
