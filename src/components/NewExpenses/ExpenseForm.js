c0800dbae31baa0db768b62503f51d280d0ddba7
Simon <omojide.o.j@gmail.com>
2025-05-28 15:14:06 +0100

    Add onSaveExpenseHandler to pass Data upward from ExpenseForm Component

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -1,21 +1,70 @@
-import React from 'react';
+import { useState } from 'react';
 import './ExpenseForm.css';
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
+	// Manage/Store user input individually
+	const [enteredTitle, setEnteredTitle] = useState('');
+	const [enteredAmount, setEnteredAmount] = useState('');
+	const [enteredDate, setEnteredDate] = useState('');
+
+	// Alternative to calling different evenHandler
+	const inputChangeHandler = (identifier, value) => {
+		if (identifier === 'title') {
+			setEnteredTitle(value);
+		} else if (identifier === 'amount') {
+			setEnteredAmount(value);
+		} else {
+			setEnteredDate(value);
+		}
+	};
+
+	const submiteHandler = (e) => {
+		e.preventDefault();
+
+		const expenseData = {
+			title: enteredTitle,
+			amount: enteredAmount,
+			date: new Date(enteredDate),
+		};
+
+		props.onSaveExpenseData(expenseData);
+    //Clear userInput
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
+	};
+
 	return (
-		<form>
+		<form onSubmit={submiteHandler}>
 			<div className="new-expense__controls">
 				<div className="new-expense__control">
 					<label>Title</label>
-					<input type="text" />
+					{/* <input type="text" onChange={TitleChangeHandler} /> */}
+					<input
+						type="text"
+            value={enteredTitle}
+						onChange={(e) => inputChangeHandler('title', e.target.value)}
+					/>
 				</div>
 				<div className="new-expense__control">
 					<label>Amount</label>
-					<input type="number" min={0.01} step={0.01} />
+					<input
+						type="number"
+            value={enteredAmount}
+						min={0.01}
+						step={0.01}
+						onChange={(e) => inputChangeHandler('amount', e.target.value)}
+					/>
 				</div>
 				<div className="new-expense__control">
 					<label>Date</label>
-					<input type="date" min="2025-01-01" max="2026-12-31" />
+					<input
+						type="date"
+            value={enteredDate}
+						min="2025-01-01"
+						max="2026-12-31"
+						onChange={(e) => inputChangeHandler('date', e.target.value)}
+					/>
 				</div>
 			</div>
 			<div className="new-expense__actions">
@@ -26,3 +75,33 @@ const ExpenseForm = () => {
 };
 
 export default ExpenseForm;
+
+// // Alternative Approach to settin useState
+// const [userInput, setUserInput] = useState({
+// 	enteredTitle: '',
+// 	enterAmount: '',
+// 	enterDate: '',
+// });
+
+// // Handler for change state
+// const TitleChangeHandler = (e) => {
+// 	setEnteredTitle(e.target.value);
+// 	// setUserInput({ ...userInput, enteredTitle: e.target.value });
+// 	// //Sets guarantee newState when depending on previous date
+// setUserInput((prevState) => {
+//   return{...prevState, enteredTitle: e.target.value}
+// })
+// 	// console.log(e.target.value);
+// // };
+
+// const AmountChangeHandler = (e) => {
+// 	setEnteredAmount(e.target.value);
+// 	// setUserInput({ ...userInput, enteredTitle: e.target.value });
+// 	// console.log(e.target.value);
+// };
+
+// const DateChangeHandler = (e) => {
+// 	setEnteredDate(e.target.value);
+// 	// setUserInput({ ...userInput, enterDate: e.target.value });
+// 	// console.log(e.target.value);
+// };
--- src/components/NewExpenses/NewExpense.js
+++ src/components/NewExpenses/NewExpense.js
@@ -3,9 +3,18 @@ import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = () => {
+	const onSaveExpenseHandler = (recievedExpenseData) => {
+		const expenseData = {
+			...recievedExpenseData,
+			id: Math.random().toString(),
+		};
+		console.log(expenseData);
+	};
+
 	return (
-		<div className='new-expense'>
-			<ExpenseForm />
+		<div className="new-expense">
+			{/* Recieves expenseData as props form Child (Expenseorm) */}
+			<ExpenseForm onSaveExpenseData={onSaveExpenseHandler} />
 		</div>
 	);
 };
